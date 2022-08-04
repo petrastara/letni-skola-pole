ddd9a67ade6305864c28290143c6c998cba723f7
Petra Stará <petrastara1@seznam.cz>
2022-08-04 12:10:27 +0200

    hotovo, snad...

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # letni-skola-pole
+
+
+
+// Jak se vytvoříí pole a co to je
+// Jak přistupujeme k prvkům pole
+// Práce s polem v cyklu
+// Metody pro práci s polem
+// - pridat na konec
+// - odebrat z konce
+// - pridat na zacatek
+// - odebrat ze zacatku
+// - vrátit kousek pole od-do
--- main.js
+++ main.js
@@ -1,19 +1,49 @@
 console.log('JavaScript funguje')
 
+// Jak se vytvoří pole a co to je
+    /* datový typ, seznam prvků*/
 
-// Jak se vytvoříí pole a co to je
+let jmena= ["Alenka", "Jája", "Pája", "Bob", "Bobek", "Pat", "Mat", "Elsa", "Anna", "Olaf" ]
 
+console.log(jmena)
 
 // Jak přistupujeme k prvkům pole
 
+console.log(jmena[9]) //z pole vypíšu prvek s indexem 9 (ty jdou od nuly)
 
-// Práce s polem v cyklu
-
+jmena[1] = "Gargamel" //měním hodnotu
+console.log(jmena)
 
 
 // Metody pro práci s polem
-// - pridat na konec
-// - odebrat z konce
-// - pridat na zacatek
-// - odebrat ze zacatku
-// - vrátit kousek pole od-do
+// - pridat na konec => PUSH
+jmena.push("Azrael")
+jmena.push("Šprýmař")
+jmena.push("Bručoun")
+console.log(jmena)
+
+// - odebrat z konce => .pop() neuvádí se parametr
+jmena.pop()
+console.log(jmena)
+
+// - pridat na zacatek => pole.UNSHIFT
+jmena.unshift("Šmoulinka")
+jmena.unshift("Silák")
+
+// - odebrat ze zacatku => POLE.SHIFT
+ jmena.shift()
+ console.log(jmena)
+
+// - vrátit kousek pole od-do => SLICE
+let novaJmena = jmena.slice(3, 11)  /*"od" zahrne, "od" už ne*/
+
+// Práce s polem v cyklu  tzv. "projít"
+
+for( let i = 0;i<4; i++  ) { //založit iterátor = počítadlo   : založím proměnou, dám tam výchozí hodnotu; když tohle platí tak se to provádí; co se stane
+    console.log ("Ahojda jahoda")
+}
+
+for( let i = 0; i < jmena.length ; i++  ) { 
+    //console.log (jmena [i])
+    console.log( "Šmoula číslo " + i + " se jmenuje " + jmena[i])
+}
