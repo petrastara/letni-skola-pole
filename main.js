d102fa5ad9b547f3ceba1c714a47db6b2ee534d2
Luděk Roleček <ludek@rolecek.cz>
2022-08-04 11:23:18 +0200

    Aktualizace podkladů

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('JavaScript funguje')
+
+
+// Jak se vytvoříí pole a co to je
+
+
+// Jak přistupujeme k prvkům pole
+
+
+// Práce s polem v cyklu
+
+
+
+// Metody pro práci s polem
+// - pridat na konec
+// - odebrat z konce
+// - pridat na zacatek
+// - odebrat ze zacatku
+// - vrátit kousek pole od-do
